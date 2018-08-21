67e545a6e553358ab9ecda8879b1f20121ef9bd9
jackzhou88 <dave888zhou@qq.com>
2018-08-21 22:02:02 +0800

    Add UI to persian.fxml.

--- persian-richapp/src/main/java/com/persian/PersianMain.java
+++ persian-richapp/src/main/java/com/persian/PersianMain.java
@@ -10,8 +10,8 @@ public class PersianMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("persian.fxml"));
+        primaryStage.setTitle("Persian Stock");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
